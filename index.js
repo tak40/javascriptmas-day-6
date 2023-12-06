5e8356111c8b3418a93bfb620ddb67598e0a2797
TK <118142360+tak40@users.noreply.github.com>
2023-12-06 14:28:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const people = ["Alice", "Bob", "Carly", "Dan", "Ed", "Ferdinand", "Ginny"]
+
+function generateSecretSantaPairs(arr) {
+    // Your code here
+}
+
+console.log(generateSecretSantaPairs(people))
+
+/**
+Example output:
+{
+    Alice: "Dan",
+    Bob: "Ferdinand",
+    Carly: "Ed",
+    Dan: "Alice",
+    Ed: "Ginny",
+    Ferdinand: "Bob",
+    Ginny: "Carly"
+}
+ */
