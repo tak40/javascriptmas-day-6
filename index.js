d2881b496f6eb1c668239434480e94fdf6ca2bce
Tak <takeshiseodev@gmail.com>
2023-12-07 09:53:49 -0500

    update: final code

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const people = ["Alice", "Bob", "Carly", "Dan", "Ed", "Ferdinand", "Ginny"]
+/** @format */
+
+const people = ['Alice', 'Bob', 'Carly', 'Dan', 'Ed', 'Ferdinand', 'Ginny']
 
 function generateSecretSantaPairs(arr) {
-    // Your code here
+    let result = {}
+    let givers = [...arr]
+    let receivers = [...arr]
+
+    for (let i = 0; i < arr.length; i++) {
+        let giver = givers[i]
+        let receiverIndex = Math.floor(Math.random() * receivers.length)
+        let receiver = receivers[receiverIndex]
+
+        // Ensure that a person doesn't get themselves
+        while (giver === receiver) {
+            receiverIndex = Math.floor(Math.random() * receivers.length)
+            receiver = receivers[receiverIndex]
+        }
+
+        result[giver] = receiver
+        // Remove the chosen receiver from the array
+        receivers.splice(receiverIndex, 1)
+    }
+    return result
 }
 
 console.log(generateSecretSantaPairs(people))
-
-/**
-Example output:
-{
-    Alice: "Dan",
-    Bob: "Ferdinand",
-    Carly: "Ed",
-    Dan: "Alice",
-    Ed: "Ginny",
-    Ferdinand: "Bob",
-    Ginny: "Carly"
-}
- */
